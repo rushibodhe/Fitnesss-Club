07a10b55a1f6d1a8cc2d4e4a8b013a2e5269dfc4
rushibodhe <bodhe.rushikesh1@gmail.com>
2022-11-08 22:51:46 +0530

    created new build and added apikey

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,21 +1,21 @@
 export const exerciseOptions = {
-    method: 'GET',
-    headers: {
-      'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
-      'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
-    }
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Key': '1b7f8e89e9msh061b02a46b1bdfap15f19fjsn4d5e980a1579',
+    'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
+  }
 };
 
 export const youtubeOptions = {
   method: 'GET',
   headers: {
-    'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
+    'X-RapidAPI-Key': '1b7f8e89e9msh061b02a46b1bdfap15f19fjsn4d5e980a1579',
     'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com'
   }
 }
 
-export const fetchData = async (url, options) =>{
-    const response = await fetch (url, options);
-    const data = await response.json();
-    return data;
+export const fetchData = async (url, options) => {
+  const response = await fetch(url, options);
+  const data = await response.json();
+  return data;
 } 
